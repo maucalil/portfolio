05921f39cba5c6aa735fd9f3eb29fb14b178b010
Maurício Calil Manfrim Lucera <m.calil-manfrim-lucera@tu-braunschweig.de>
2025-04-01 15:17:55 -0300

    refactor: change navbar style and behavior on mobile

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var navBtn = document.querySelector(".nav-btn")
 var navLinks = document.querySelector(".nav-links")  
-var navLinksList = document.querySelector(".nav-links li");
+var navLinksList = document.querySelectorAll(".nav-links li"); 
 
-navBtn.addEventListener("click", function () {
-    this.classList.toggle("click");
+navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("click");
     navLinks.classList.toggle("open");
-})
+});
+
+navLinksList.forEach(li => 
+    li.addEventListener("click", () => {
+        navBtn.classList.remove("click");
+        navLinks.classList.remove("open")
+    })
+);
 
 var typed = new Typed(".input", {
     strings: ["Fullstack Developer", "Backend Developer"],
